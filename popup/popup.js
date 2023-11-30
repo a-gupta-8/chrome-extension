feb6221d3b2c90867267fa0b03c8d3d1e1d56d61
agupta8 <avibox9511051@gmail.com>
2023-11-29 19:21:55 -0800

    #1 enabled toggle between text with single button

--- popup/popup.js
+++ popup/popup.js
@@ -3,7 +3,11 @@ document.getElementById("myButton").addEventListener("click", replaceText);
 function replaceText() {
   var originalParagraph = document.getElementById("originalText");
   var replacementParagraph = document.getElementById("replacementText");
-
-  originalParagraph.style.display = "none";
-  replacementParagraph.style.display = "block";
+  if (originalParagraph.style.display === "none") {
+    originalParagraph.style.display = "block";
+    replacementParagraph.style.display = "none";
+  } else {
+    originalParagraph.style.display = "none";
+    replacementParagraph.style.display = "block";
+  }
 }
